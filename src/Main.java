1f9c1be53ad7dfc60c04c9ed7e80bf12acc3465e
nemo9955 <nemo9955@gmail.com>
2013-04-07 19:04:07 +0300

    adaugat slick + putina manipulare de hexagoane

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,63 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class Main {
+public class Main extends BasicGame {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
+	private static String title = "hexagon";
+	private int marime = 30;
+
+	public Main(String title) {
+		super(title);
+	}
+
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Main(title));
+		app.setDisplayMode(800, 600, false);
+		app.setTargetFrameRate(60);
+		app.setResizable(true);
+		app.setShowFPS(true);
+		app.start();
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer argcg0, Graphics g) throws SlickException {
+		for (float i = 50; i <= 300; i += pit(marime)*2)
+			for (int j = 50; j <= 300; j += marime*2) {
+				if (((i - marime) / marime) % 2 == 0)
+					hex(j, i, marime, g);
+				else
+					hex(j - (marime / 2), i, marime, g);
+			}
+	}
+
+	private void hex(float x, float y, int mar, Graphics g) {
+		g.drawLine(x - (mar / 2), y - pit(mar), x + (mar / 2), y - pit(mar));
+		g.drawLine(x - (mar / 2), y + pit(mar), x + (mar / 2), y + pit(mar));
+
+		g.drawLine(x - mar, y, x - (mar / 2), y - pit(mar));
+		g.drawLine(x + mar, y, x + (mar / 2), y - pit(mar));
+
+		g.drawLine(x - mar, y, x - (mar / 2), y + pit(mar));
+		g.drawLine(x + mar, y, x + (mar / 2), y + pit(mar));
+	}
+
+	private float pit(float lun) {
+		return (float) ((float) (lun / 2) * Math.sqrt(3));
 	}
 
 }
